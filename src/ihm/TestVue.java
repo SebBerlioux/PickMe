c1a0880573e9d77bf98c22541f8af08c348f1ec5
sebastien <sebastien.berlioux@etu.univ-nantes.fr>
2018-05-29 15:02:04 +0200

    ajout vue creation voyage

---
+++ src/ihm/TestVue.java
@@ -0,0 +1,9 @@
+package IHM;
+
+public class TestVue {
+
+	public static void main(String[] args) {
+		VueCreationVoyage vue = new VueCreationVoyage();
+	}
+
+}
---
+++ src/ihm/VueCreationVoyage.java
@@ -0,0 +1,80 @@
+package IHM;
+
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+
+public class VueCreationVoyage extends JFrame {
+	
+	public JLabel titre;
+	public JLabel depart;
+	public JLabel date;
+	public JLabel heureDepart;
+	public JLabel nbPlace;
+	public JTextField boxDepart;
+	public JTextField txtFDate1;
+	public JTextField txtFDate2;
+	public JTextField txtFDate3;
+	public JTextField txtFHeure1;
+	public JTextField txtFHeure2;
+	public JTextField txtFPlace;
+	public JButton boutton;
+	
+	public VueCreationVoyage() {
+		this.titre = new JLabel("Créer un voyage");
+		this.depart = new JLabel("Départ: ");
+		this.date = new JLabel("Date: ");
+		this.heureDepart = new JLabel("Heure de départ: ");
+		this.nbPlace = new JLabel("Nombre de place(s): ");
+		this.boxDepart = new JTextField(10);
+		this.txtFDate1 = new JTextField(2);
+		this.txtFDate2 = new JTextField(2);
+		this.txtFDate3 = new JTextField(4);
+		this.txtFHeure1 = new JTextField(2);
+		this.txtFHeure2 = new JTextField(2);
+		this.txtFPlace = new JTextField(2);
+		this.boutton = new JButton("Suivant");
+		
+		boxDepart.setEditable(true);
+		txtFDate1.setEditable(true);
+		txtFDate2.setEditable(true);
+		txtFDate3.setEditable(true);
+		txtFHeure1.setEditable(true);
+		txtFHeure2.setEditable(true);
+		txtFPlace.setEditable(true);
+		
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		
+		this.add(titre);
+		
+		panel1.add(depart);
+		panel1.add(boxDepart);
+		
+		panel2.add(date);
+		panel2.add(txtFDate1);
+		panel2.add(txtFDate2);
+		panel2.add(txtFDate3);
+		
+		panel3.add(heureDepart);
+		panel3.add(txtFHeure1);
+		panel3.add(txtFHeure2);
+		
+		panel4.add(nbPlace);
+		panel4.add(txtFPlace);
+		
+		this.add(boutton);
+
+		setSize(1000,500);
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+		//setContentPane();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
