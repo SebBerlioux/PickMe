50af25b4c836bb402f654be649160a2871776c6f
Antoine MARJAULT <antoine.marjault@etu.univ-nantes.fr>
2018-05-29 19:06:57 +0200

    maj

--- src/ihm/VueVoyages.java
+++ src/ihm/VueVoyages.java
@@ -1,17 +1,70 @@
 package ihm;
 
-import javax.swing.JPanel;
+import java.awt.Component;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
-public class VueVoyages extends JPanel{
+public class VueVoyages extends JFrame{
 	
 	public JTable myTable;
 	public String[] columnsNames = {"Trajet", "Date", "Conducteur", "Prix", "Etat", "Détails"};
-	public Object[][] data = {{"bite","bite","bite","bite","bite","bite"}};
+	public Object[][] data = {{"xxx","xxx","xxx","xxx","xxx"},{"xxx","xxx","xxx","xxx","xxx"}};
+	public JButton button = new JButton();
 			
 	public VueVoyages() {
 		this.myTable = new JTable(data, columnsNames);
-		this.add(myTable);
+		
+		myTableModel model = new myTableModel();
+		myTable.setModel(model);
+		myTable.getColumn("Détails").setCellRenderer(new ButtonRenderer());
+		myTable.getColumn("Détails").setCellEditor(new ButtonEditor(new JCheckBox()));
+		
+		add(new JScrollPane(myTable));
+	}
+
+	class ButtonRenderer extends JButton implements TableCellRenderer {
+
+		public ButtonRenderer() {
+			setOpaque(true);
 		}
+
+		public Component getTableCellRendererComponent(JTable table, Object value, 
+				boolean isSelected, boolean hasFocus, int row, int column) {
+			button.setText("Détails");
+			return button;
+		}
+
+	}
+
+	class ButtonEditor extends DefaultCellEditor {
 	
+		public ButtonEditor(JCheckBox checkbox) {
+			super(checkbox);
+		}
+
+		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+			button.setText("Détails");
+			return button;
+		}
+	}
+
+	public class myTableModel extends DefaultTableModel {
+	
+		JButton button=new JButton();
+
+		public myTableModel() {
+			super(data,columnsNames);
+		}
+
+		public boolean isCellEditable(int row,int cols) {		
+			return true;
+		}
+
+	}         
 }
