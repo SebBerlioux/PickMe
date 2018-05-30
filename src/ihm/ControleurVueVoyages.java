f135a56c399979481fe50e03be4629c8b8200025
Antoine MARJAULT <antoine.marjault@etu.univ-nantes.fr>
2018-05-30 22:52:14 +0200

    affichage des voyages quasi ok

--- src/ihm/ControleurVueVoyages.java
+++ src/ihm/ControleurVueVoyages.java
@@ -1,6 +1,7 @@
 package ihm;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.AbstractAction;
@@ -13,17 +14,16 @@ public class ControleurVueVoyages {
 	
 	public ControleurVueVoyages(Utilisateur utilisateur) {
 		this.utilisateur = utilisateur;
-		System.out.println(utilisateur.mesVoyages().size());
+		ArrayList<String> voyages = utilisateur.mesVoyages();
 
-		Object[][] data = new Object [utilisateur.mesVoyages().size()][5];
+		Object[][] data = new Object [voyages.size()][5];
 		
 		for(int i =0; i < utilisateur.mesVoyages().size(); i++) {
-			   data[i][0] = utilisateur.mesVoyages().get(0) + ">" + utilisateur.mesVoyages().get(1);
-			   System.out.println(data[i][0]);
-			   data[i][1] = utilisateur.mesVoyages().get(2);
-			   data[i][2] = utilisateur.mesVoyages().get(3) + " " + utilisateur.mesVoyages().get(4);
-			   data[i][2] = utilisateur.mesVoyages().get(5);
-			   data[i][2] = utilisateur.mesVoyages().get(6);
+			   data[i][0] = voyages.get(0) + ">" + voyages.get(1);
+			   data[i][1] = voyages.get(2);
+			   data[i][2] = voyages.get(3) + " " + voyages.get(4);
+			   data[i][3] = voyages.get(5);
+			   data[i][4] = voyages.get(6);
 		}
 		
 		this.vue = new VueVoyages(data);
