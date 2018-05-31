b524e49b2c69be9ae6c9ef731d7b91082c59bb65
Antoine MARJAULT <antoine.marjault@etu.univ-nantes.fr>
2018-05-31 13:18:15 +0200

    update ok :)

--- src/ihm/ControleurVueVoyages.java
+++ src/ihm/ControleurVueVoyages.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.AbstractAction;
-import javax.swing.JTable;
 
 import GestionDonnees.Utilisateur;
 import java.util.Observer;
@@ -16,30 +15,19 @@ public class ControleurVueVoyages implements Observer{
 	ModelSousVueVoyage model;
 	
 	public ControleurVueVoyages(Utilisateur utilisateur) {
-<<<<<<< HEAD
 		this.vue = new VueVoyages();
-=======
-		this.utilisateur = utilisateur;
-		this.utilisateur.addObserver(this);
->>>>>>> a090c19b1c49ea6877db403c71a19acc90b4ceb2
 		
 		ArrayList<String> voyages = utilisateur.mesVoyages();
 		Integer nbVoyages = utilisateur.getNbVoyages();
+
 		Object[][] data = new Object [nbVoyages][5];
-		if(nbVoyages != 0) {
-			System.out.println("nbVoyages:" + nbVoyages);
-			
-			for(int i =0; i < nbVoyages; i++) {
-			   data[i][0] = voyages.get(0+i*7) + ">" + voyages.get(1+i*7);
-			   data[i][1] = voyages.get(2+i*7);
-			   data[i][2] = voyages.get(3+i*7) + " " + voyages.get(4+i*7);
-			   data[i][3] = voyages.get(5+i*7);
-			   data[i][4] = voyages.get(6+i*7);
-			}
-			try {
-				this.vue.data = data;
-			}catch(NullPointerException e){
-			}
+		
+		for(int i =0; i < nbVoyages; i++) {
+				   data[i][0] = voyages.get(0+i*7) + ">" + voyages.get(1+i*7);
+				   data[i][1] = voyages.get(2+i*7);
+				   data[i][2] = voyages.get(3+i*7) + " " + voyages.get(4+i*7);
+				   data[i][3] = voyages.get(5+i*7);
+				   data[i][4] = voyages.get(6+i*7);
 		}
 		
 		this.model = new ModelSousVueVoyage(data);
@@ -54,7 +42,7 @@ public class ControleurVueVoyages implements Observer{
 		this.utilisateur.addObserver(this);
 	}
 	
-	public class ActionDétails extends AbstractAction implements Observer{
+	public class ActionDetails extends AbstractAction implements Observer{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -72,7 +60,6 @@ public class ControleurVueVoyages implements Observer{
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-<<<<<<< HEAD
 		System.out.println("===========================UPDATE==========================");
 		ArrayList<String> voyages = utilisateur.mesVoyages();
 		Integer nbVoyages = utilisateur.getNbVoyages();
@@ -94,25 +81,6 @@ public class ControleurVueVoyages implements Observer{
 		{
 			this.vue.panelMain.add(new ControleurSousVueVoyage(this.model, i).vue);
 		}
-=======
-		ArrayList<String> voyages = this.utilisateur.mesVoyages();
-		Integer nbVoyages = this.utilisateur.getNbVoyages();
-
-		Object[][] data = new Object [nbVoyages][5];
->>>>>>> a090c19b1c49ea6877db403c71a19acc90b4ceb2
 		
-		for(int i =0; i < nbVoyages; i++) {
-				   data[i][0] = voyages.get(0+i*7) + ">" + voyages.get(1+i*7);
-				   data[i][1] = voyages.get(2+i*7);
-				   data[i][2] = voyages.get(3+i*7) + " " + voyages.get(4+i*7);
-				   data[i][3] = voyages.get(5+i*7);
-				   data[i][4] = voyages.get(6+i*7);
-		}
-		this.vue.removeAll();
-		this.vue.data = data;
-		//this.vue = new VueVoyages(this.vue.data);
-		this.vue.revalidate();
-		this.vue.repaint(this.vue.getGraphics());
-		this.vue.updateUI();
 	}
 }
